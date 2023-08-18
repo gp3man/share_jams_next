01f2cb4516f21e8d824c7e812a530600a0f399b5
Greg <greg.pounds3@gmail.com>
2023-08-18 14:51:41 -0500

    auth is working

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home</h1>
       {session ? (
-        <p>Your signed in as {session?.user}</p>
+        <p>Your signed in as {session?.user.name}</p>
       ):(<h1>You need to sign in!!</h1>)}
     </main>
   )
