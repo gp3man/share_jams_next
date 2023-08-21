82c185c5c3fed536eff163a98eacad1ceced2447
Greg <greg.pounds3@gmail.com>
2023-08-21 01:38:33 -0500

    shot in the dark

--- pages/login.js
+++ pages/login.js
@@ -4,26 +4,31 @@ import Image from "next/image";
 import "../app/globals.css";
 const login = ({ providers }) => {
   return (
-    <div className="flex flex-col h-screen justify-around p-12 items-center bg-black">
+    <div className="flex flex-col h-screen justify-around p-12 items-center bg-black text-slate-50">
       <Image alt="LogoWhite" src={LogoWhite} width={300} className="w-1/3" />
+      <div className="text-center">
       <p className="text-xl">Welcome, to ShareJams V2</p>
       <p className="text-lg">This is a remote app!</p>
+      </div>
+      <div>
       <p className="text-xs font-bold">
         In order to fully enjoy this app you will have to do the following:
       </p>
-      <ul className="text-xs font-thin">
-        <li>Login to a device with the spotify app.</li>
-        <li>Press play on a song of choice then pause it.</li>
-        <li>If you are not a Spotify premium member some features may not be available due to the nature of the api.</li>
+      <ol className="text-xs font-thin list-disc list-inside">
+        <li>Login to a device using the spotify app.</li>
+        <li>Click play on a song of choice, then pause it.</li>
+        <li>**NOTE** Only Spotify Premium members have full access to the api calls.</li>
         <li>Jam on!</li>
-      </ul>
+      </ol>
+
+      </div>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
-            className="bg-[#31ca6ce0] text-slate-500 rounded-3xl p-3"
+            className="bg-[#2aea73] text-slate-50 rounded-3xl p-3"
             onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
-            Login with {provider.name}
+            Login to {provider.name}
           </button>
         </div>
       ))}
